1d47bd3ba79ee032d2fa695299122312fa902ba2
Antoine Hoarau <hoarau.robotics@gmail.com>
2016-07-11 15:57:17 +0200

    remove prints

--- emergency_stop_publisher.py
+++ emergency_stop_publisher.py
@@ -15,19 +15,20 @@ rospy.init_node("safety_stop")
 stop_zone = 1.4 # 1.4m
 hysteresis = 0.2
 hysteresis_time = 1.0 # 1s
-oldest_ok = -1 
+oldest_ok = -1
 
 def distanceCB(msg):
-    print msg
+    # print msg
     if msg.data < stop_zone :
-        print "STOP"
+        # print "STOP"
         pub.publish(True)
     elif msg.data < stop_zone + hysteresis :
-        print "Neutral zone"
+        pass
+        # print "Neutral zone"
     else :
-        print 'OK'
-        pub.publish(False)         
-        
+        # print 'OK'
+        pub.publish(False)
+
 
 sub = rospy.Subscriber("/sick_proc/min_dist_to_laser",Float32,callback=distanceCB,tcp_nodelay=True,queue_size=10)
 pub = rospy.Publisher("/robot_emergency_stopped",Bool,tcp_nodelay=True,queue_size=1)
